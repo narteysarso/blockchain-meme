4d09d072483e539a69669d53645a605f68b8fbd1
narteysarso <narteysarso@gmail.com>
2022-01-15 15:03:04 +0000

    Add header

--- src/components/Layout/HomeLayout.jsx
+++ src/components/Layout/HomeLayout.jsx
@@ -1,10 +1,17 @@
-import { Layout} from "antd";
+import { Layout, Typography} from "antd";
 import Footer from "../Footer";
+import Logo from "../Logo";
 
 export default function HomeLayout ({children}){
     return (
         <Layout className="mainLayout">
-            <Layout.Content className="main">
+            <Layout.Header className="transparent-bg p-1">
+                <div className="d-flex j-left a-center">
+                    <Logo /> 
+                    <Typography.Title level={3} className="pl-1"> BlockMeme </Typography.Title>
+                </div>
+            </Layout.Header>
+            <Layout.Content className="main p-1">
                 {children}
             </Layout.Content>
             <Layout.Footer>
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+export default function Logo(){
+    return(
+        <img src="/images/logo.jpg" width={46} alt="icon"/>
+    )
+}
