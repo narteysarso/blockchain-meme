83b232d0c913d2bd68bab93c4ff54243b895e410
narteysarso <narteysarso@gmail.com>
2022-01-20 18:51:00 +0000

    Ropsten test network configuration

--- src/components/Layout/HomeLayout.jsx
+++ src/components/Layout/HomeLayout.jsx
@@ -5,11 +5,12 @@ import Logo from "../Logo";
 export default function HomeLayout ({children}){
     return (
         <Layout className="mainLayout">
-            <Layout.Header className="transparent-bg p-1">
+            <Layout.Header className="transparent-bg p-1 d-flex j-between">
                 <div className="d-flex j-left a-center">
                     <Logo /> 
                     <Typography.Title level={3} className="pl-1"> BlockMeme </Typography.Title>
                 </div>
+                    <Typography.Title level={5} style={{alignSelf: "end", color: "tomato"}}>This DApp works only on the Ropsten Test Network</Typography.Title>
             </Layout.Header>
             <Layout.Content className="main p-1">
                 {children}
